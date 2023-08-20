de6d955ba9c6f665239eb10dd3079bede2ae23ed
thierry SAUTIERE <tsautiere@aol.com>
2023-08-20 13:54:05 +0000

    modification de la structure

--- src/structures/premiere_techno.js
+++ src/structures/premiere_techno.js
@@ -1,7 +1,13 @@
-export default {    
+export default {
     'Suites numériques, suites arithmétiques': {
         "image": 1,
-        "thèmes fiche": [['#', '#']],
+        "thèmes fiche": [
+            ['Calculs de termes', 'pdf/premiere_spe/suites/Suites(calcusdetermes,etc...).pdf'],
+            ['Comparaison SA|SG', 'pdf/premiere_spe/suites/Suites(comparaisonsuitesarithmetiquesetgeometriques).pdf'],
+            ['Suites arithmétiques(divers)', 'pdf/premiere_spe/suites/Suitesarithmetiques(divers).pdf'],
+            ['Suites arithmétiques(problèmes)', 'pdf/premiere_spe/suites/Suitesarithmetiques(problemes).pdf'],
+            ['Graphiques', 'pdf/premiere_spe/suites/Suitesetgraphiques.pdf'],
+        ],
         "thèmes ligne": [["Suites récurrentes : niveau n + 1", "premiere_techno/Suites/Suites_recurrentes/Niveau_nPlus1/Suites_nPlus1.html"],
         ["Suites récurrentes : niveau n + 2", "premiere_techno/Suites/Suites_recurrentes/Niveau_nPlus2/Suites_nPlus2.html"],
         ["Suites arithmétiques : calcul de terme (à partir de u0)", "premiere_techno/Suites/Suites_arithmetiques/Calcul_de_terme/A_partir_de_u0/Suite_Ari_Calcul_De_Terme_u0.html"],
@@ -12,55 +18,83 @@ export default {
         ["Suites arithmétiques : problèmes (à partir de u0)", "premiere_techno/Suites/Suites_arithmetiques/Problemes/Problemes_u0/Problemes_Suites_Ari_u0.html"],
         ["Suites arithmétiques : problèmes (à partir de u1)", "premiere_techno/Suites/Suites_arithmetiques/Problemes/Problemes_u1/Problemes_Suites_Ari_u1.html"]]
     },
-    'Fonctions : généralités':{
+    'Fonctions : généralités': {
         "image": 2,
-        "thèmes fiche": [['#', '#']],
+        "thèmes fiche": [[
+            ['Questions sur les courbes', 'pdf/premiere_spe/fonctions/Courbesetquestions.pdf'],
+            ['Dérivées et tangentes', 'pdf/premiere_spe/fonctions/Deriveesettangentes.pdf'],
+            ['Dérivées et tableaux de variations', 'pdf/premiere_spe/fonctions/Derivesettableauxdevariations.pdf'],
+            ['Equations de droites', 'pdf/premiere_spe/fonctions/Equationsdedroites.pdf'],
+            ['Equations de tangentes(entrainement)', 'pdf/premiere_spe/fonctions/Equationsdetangentes(entrainement).pdf'],
+            ['Equations de tangentes(calculs)', 'pdf/premiere_spe/fonctions/Equationsdetangentes(par calculs).pdf'],
+            ['Tableaux de variations(questions)', 'pdf/premiere_spe/fonctions/Tableauxdevariations(questions).pdf']
+        ]],
         "thèmes ligne": [["Images et antécédents", "premiere_techno/Fonctions/Images_antecedents/Images_Antecedents.html"]
-    ]
+        ]
     },
-    'Probabilités conditionnelles':{
+    'Probabilités conditionnelles': {
         "image": 3,
-        "thèmes fiche": [['#', '#']],
-        "thèmes ligne": [  ]
+        "thèmes fiche": [
+            ['Arbres de probabilités', 'pdf/premiere_spe/probabilités/Arbresdeprobabilites1ST.pdf'],
+            ['Fréquences et tableaux', 'pdf/premiere_spe/probabilités/Frequencesettableaux.pdf'],
+            ['Variables aléatoires', 'pdf/premiere_spe/probabilités/Probabilites(variablesaleatoires).pdf'],
+            ['Proportions', 'pdf/premiere_spe/probabilités/Proportions_probabilites.pdf'],
+        ],
+        "thèmes ligne": []
     },
-    'Fonctions de degré 2':{
+    'Fonctions de degré 2': {
         "image": 4,
-        "thèmes fiche": [['#', '#']],
+        "thèmes fiche": [
+            ['Fonctions de degré 2(calculs)', 'pdf/premiere_spe/fonctions/Fonctionsdedegre2(aspectcalculs).pdf'],
+            ['Fonctions de degré 2(graphiques)', 'pdf/premiere_spe/fonctions/Fonctionsdedegre2(aspectgraphique).pdf']
+        ],
         "thèmes ligne": [["Équations", "premiere_techno/Fonctions/Fonctions_degre_2/Equations/EqsDegre2.html"],
         ["Avec Beta", "premiere_techno/Fonctions/Fonctions_degre_2/Avec_Beta/Avec_Beta.html"],
         ["Forme développée", "premiere_techno/Fonctions/Fonctions_degre_2/Forme_developpee/Forme_developpee.html"],
         ["Forme factorisée", "premiere_techno/Fonctions/Fonctions_degre_2/Forme_factorisee/Forme_Factorisee.html"],
         ["Sommet", "premiere_techno/Fonctions/Fonctions_degre_2/Sommet/Sommet.html"]
-    ]
+        ]
     },
-    'Suites géométriques':{
+    'Fonctions de degré 3': {
+        "image": 9,
+        "thèmes fiche": [
+            ['Fonctions de degré 3(calculs)', 'pdf/premiere_spe/fonctions/Fonctionsdedegre3(calculs).pdf'],
+            ['Fonctions de degré 3(graphiques)', 'pdf/premiere_spe/fonctions/Fonctionsdedegre3(graphiques).pdf'],
+        ],
+        "thèmes ligne": [
+        ]
+    },
+    'Suites géométriques': {
         "image": 5,
-        "thèmes fiche": [['#', '#']],
+        "thèmes fiche": [
+            ['Suites géométriques(termes|raison)', 'pdf/premiere_spe/suites/Suitesgeometriques(calculsdetermesetderaison).pdf'],
+            ['Suites géométriques(divers)', 'pdf/premiere_spe/suites/Suitesgeometriques(divers).pdf']
+        ],
         "thèmes ligne": [["Suites géométriques : calcul de terme (à partir de u0)", "premiere_techno/Suites/Suites_geometriques/Calcul_de_terme/A_partir_de_u0/Suite_Geo_Calcul_De_Terme_U0.html"],
         ["Suites géométriques : calcul de terme (à partir de u quelconque)", "premiere_techno/Suites/Suites_geometriques/Calcul_de_terme/A_partir_de_uQuelconque/Suite_Geo_Calcul_De_Terme_UQcque.html"],
         ["Suites géométriques : calcul de raison", "premiere_techno/Suites/Suites_geometriques/Calcul_de_raison/Calcul_de_raison_geo.html"],
         ["Suites géométriques : problème", "premiere_techno/Suites/Suites_geometriques/Problemes/Problemes_Suites_Geo.html"],
         ["Suites géométriques ou arithmétiques : problème", "premiere_techno/Suites/Suites_geometriques/Problemes_suites_quelconques/Problemes_Suites_Aleatoire.html"]
-    ]
+        ]
     },
-    'Dérivées':{
+    'Dérivées': {
         "image": 6,
         "thèmes fiche": [['#', '#']],
         "thèmes ligne": [["Calculs de dérivées", "premiere_techno/Fonctions/Derivees/Calcul_de_derivees/Calcul_de_derivees.html"],
         ["Équations de tangentes", "premiere_techno/Fonctions/Derivees/Equations_de_tangentes/Eqs_Tgtes.html"],
         ["Taux de variation (avec une fonction)", "premiere_techno/Fonctions/Derivees/Taux_de_variations/Avec_une_fonction/Taux_de_variation_fonction.html"]
-    ]
+        ]
     },
-    'Probabilités : arbres et schéma de Bernoulli':{
+    'Probabilités : arbres et schéma de Bernoulli': {
         "image": 7,
         "thèmes fiche": [['#', '#']],
         "thèmes ligne": [["Schéma de Bernoulli : calculs de probabilités", "premiere_techno/Probabilites/Schema_de_Bernoulli/Calcul_de_probabilites/Bernoulli_Calcul_de_Probs.html"]
-    ]
+        ]
     },
-    'Fonctions de degré 3':{
+    'Fonctions de degré 3': {
         "image": 8,
         "thèmes fiche": [['#', '#']],
         "thèmes ligne": [["Forme développée", "premiere_techno/Fonctions/Fonctions_degre_3/Forme_developpee/Forme_developpee.html"]
-       ]
+        ]
     }
 }
